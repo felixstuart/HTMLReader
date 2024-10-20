1f9847e64a1eb85d66cb10cf33c7cf0dddd36827
felixstuart <148880497+felixstuart@users.noreply.github.com>
2024-10-20 11:14:55 -0400

    nice layout

--- src/HtmlRead.java
+++ src/HtmlRead.java
@@ -1,25 +1,77 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 
 public class HtmlRead {
+    private JFrame mainFrame;
+    private int WIDTH = 1000;
+    private int HEIGHT = 700;
+
+    public HtmlRead() {
+        prepareGUI();
+//        getLinks("https://www.milton.edu");
+    }
 
     public static void main(String[] args) {
         HtmlRead htmlRead = new HtmlRead();
+        htmlRead.show();
     }
 
-    public HtmlRead() {
+    private void prepareGUI() {
+        mainFrame = new JFrame();
+        mainFrame.setSize(WIDTH, HEIGHT);
+        mainFrame.setLayout(new BorderLayout());
 
+        JPanel urlBarLayout = new JPanel();
+        urlBarLayout.setLayout(new GridBagLayout());
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+
+        JTextField urlField = new JTextField();
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 0;
+        urlBarLayout.add(urlField, c);
+
+        JButton goButton = new JButton("Go");
+        c.weightx = 0;
+        c.gridx = 1;
+        c.gridy = 0;
+        urlBarLayout.add(goButton, c);
+
+        JTextField searchField = new JTextField();
+        c.gridx = 0;
+        c.gridy = 1;
+        c.weightx = 0.5;
+        urlBarLayout.add(searchField, c);
+
+
+
+        mainFrame.add(urlBarLayout, BorderLayout.NORTH);
+
+    }
+
+
+    private void getLinks(String location) {
         try {
             System.out.println();
-            URL url = new URL("https://www.milton.edu/");
+            System.out.println(location);
+            URL url = new URL(location);
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(url.openStream())
             );
             String line;
-            while ( (line = reader.readLine()) != null ) {
-                if(line.contains("href")) {
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("href")) {
                     int indexHttp = line.indexOf("href") + 6;
                     String newLine = line.substring(indexHttp);
 
@@ -27,17 +79,38 @@ public class HtmlRead {
                     int oEnd = newLine.indexOf("'");
 
                     if (oEnd > end) {
-                        System.out.println(newLine.substring(0,oEnd));
+                        System.out.println(newLine.substring(0, oEnd));
                     } else {
-                        System.out.println(newLine.substring(0,end));
+                        System.out.println(newLine.substring(0, end));
+
                     }
                 }
             }
             reader.close();
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             System.out.println(ex);
         }
+    }
 
+    private void show() {
+        mainFrame.setVisible(true);
+    }
+
+
+    private class GoButtonListener implements ActionListener {
+        private JTextArea urlField;
+        private JTextArea searchField;
+
+        public GoButtonListener(JTextArea url, JTextArea searchBar) {
+            urlField = url;
+            searchField = searchBar;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("button clicked");
+            getLinks(urlField.getText());
+        }
     }
 
 }
